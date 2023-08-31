dd6c1ea20eb8eb78b1e13a5e12ad7e22782d810e
Hugo Miquel <hugomiquel498@gmail.com>
2023-09-01 00:25:46 +0200

    Pushing main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include "IRSender.h"
 #include "WifiReceiver.h"
 
+const int irPin = 2;
+const int wifiCePin = 7;
+const int wifiCsnPin = 8;
+
+IRSender irSender(irPin);
+WiFiReceiver wifiReceiver(wifiCePin, wifiCsnPin);
+
 void setup() {
+  Serial.begin(115200);
+
+  irSender.begin();
+  wifiReceiver.begin();
 }
 
 void loop() {
+  irSender.sendSignal(1); // Replace with the desired puceId
+
+  if (wifiReceiver.available()) {
+    char message[32] = "";
+    wifiReceiver.readMessage(message, sizeof(message));
+    Serial.println("Received WiFi message: " + String(message));
+  }
 }
